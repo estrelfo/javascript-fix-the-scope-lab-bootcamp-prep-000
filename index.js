053cba43703e2ab5f63a40442fad6d6c408bafbf
bruno refos <brunesrefuso2@gmail.com>
2018-02-16 14:43:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var animal = 'dog';
 
 function myAnimal() {
+  var animal = 'dog';
+
   return animal;
 }
 
@@ -9,7 +10,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   
-  
+  var animal = 'dog';
   return animal;
 }
 
